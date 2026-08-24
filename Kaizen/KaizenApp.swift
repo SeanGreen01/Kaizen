1078ec679a3424fea931dc93a5b951b6508bb077
sgreen1947 <73635914+sgreen1947@users.noreply.github.com>
2026-08-24 20:40:14 +0100

    Initial Commit

---
+++ Kaizen/KaizenApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaizenApp.swift
+//  Kaizen
+//
+//  Created by Sean Green on 24/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct KaizenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KaizenTests/KaizenTests.swift
@@ -0,0 +1,17 @@
+//
+//  KaizenTests.swift
+//  KaizenTests
+//
+//  Created by Sean Green on 24/08/2026.
+//
+
+import Testing
+@testable import Kaizen
+
+struct KaizenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
